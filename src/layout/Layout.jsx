674baf4dbeeba944551bfdfbff1f148af5d88889
jason <jaml08@hotmail.com>
2026-01-17 07:47:41 -0500

    feat: action bar integrado al layout general

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import { FloatingActionBar } from "./FloatingActionBar";
 
 export const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main className="w-full">{children}</main>
+      <main className="w-full bg-[#16181D]">{children}</main>
+      <FloatingActionBar />
       <Footer />
     </>
   );
