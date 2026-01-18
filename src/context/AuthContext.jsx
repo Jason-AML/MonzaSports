b1184579ca79e84b895442255e7e074c47635fd1
jason <jaml08@hotmail.com>
2026-01-17 19:21:46 -0500

    feat: contexto y proveedor que obtiene el usuario y escucha la suscripcion

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "../backend/Client";
+
+export const authContext = createContext();
+
+export const useAuthContext = () => {
+  const context = useContext(authContext);
+  if (!context) throw new Error("No hay contexto");
+  return context;
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(true);
+
+  useEffect(() => {
+    let listener;
+
+    const initializeAuth = async () => {
+      try {
+        const { data } = await supabase.auth.getSession();
+
+        setUser(data?.session?.user ?? null);
+
+        listener = supabase.auth.onAuthStateChange((_event, session) => {
+          setUser(session?.user ?? null);
+        });
+      } catch (err) {
+        console.error("Error en AuthContext:", err);
+        setLoadingAuth(false);
+      }
+    };
+
+    initializeAuth();
+
+    return () => {
+      if (listener?.subscription) listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <authContext.Provider value={{ user, loadingAuth }}>
+      {children}
+    </authContext.Provider>
+  );
+};
