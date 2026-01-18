3218471304c5c13374c4c5dd2cdf5862635ef2da
jason <jaml08@hotmail.com>
2026-01-17 19:17:23 -0500

    feat: nuevas rutas de registro y login

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,8 @@ import { Home } from "../pages/Home";
 import { Collections } from "../pages/Collections";
 import { Details } from "../pages/Details";
 import { Reservation } from "../pages/Reservation";
+import { Register } from "../pages/Register";
+import { Login } from "../pages/Login";
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -11,6 +13,8 @@ export const AppRoutes = () => {
         <Route path="/collections" element={<Collections />} />
         <Route path="/details" element={<Details />} />
         <Route path="/reservation" element={<Reservation />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
