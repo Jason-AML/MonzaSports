46522953bf075eec285fc7dea7ffa4a622e96cb3
jason <jaml08@hotmail.com>
2026-01-17 19:18:11 -0500

    feat: importacion y uso de toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { ToastContainer } from "react-toastify";
 import { AppRoutes } from "./routes/AppRoutes";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
       <AppRoutes />
     </>
   );
