e845a7442511724b16eee62c72fbcac9a2ece895
jason <jaml08@hotmail.com>
2026-02-01 14:27:01 -0500

    feat: Proveedor de contexto de vehiculos

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,13 @@ import "./index.css";
 import "cally";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthContextProvider } from "./context/AuthContext";
+import { VehicleContextProvider } from "./context/VehicleContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <VehicleContextProvider>
+        <App />
+      </VehicleContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 );
