de9c4b6a3e735f627dbe943f526c90fc0daaef8f
jason <jaml08@hotmail.com>
2026-02-04 11:55:35 -0500

    feat: Permisos de rutas para las paginas privadas y publicas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,10 @@ import { Details } from "../pages/Details";
 import { Reservation } from "../pages/Reservation";
 import { Register } from "../pages/Register";
 import { Login } from "../pages/Login";
+import PublicRoute from "./PublicRoutes";
+import { Dashboard } from "../pages/Dashboard";
+import PrivateRoutes from "./PrivateRoutes";
+
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -13,8 +17,25 @@ export const AppRoutes = () => {
         <Route path="/collections" element={<Collections />} />
         <Route path="/details/:id" element={<Details />} />
         <Route path="/reservation/:id" element={<Reservation />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
