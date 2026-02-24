b27f6bddea772ca40673e4cb3fe3a6cdd74905a2
jason <jaml08@hotmail.com>
2026-02-24 03:27:17 -0500

    feat: uso del componente ScrollToTop

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -8,35 +8,39 @@ import { Login } from "../pages/Login";
 import PublicRoute from "./PublicRoutes";
 import { Dashboard } from "../pages/Dashboard";
 import PrivateRoutes from "./PrivateRoutes";
+import ScrollToTop from "../components/scroll/ScrollToTop";
 
 export const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collections" element={<Collections />} />
-        <Route path="/details/:id" element={<Details />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/reservation/:id" element={<Reservation />} />
-        </Route>
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/reservation/:id" element={<Reservation />} />
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
